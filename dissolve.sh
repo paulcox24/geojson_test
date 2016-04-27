656791940b6c98022d07a2ecfb8c235b0b61a86b
Paul Cox <paulcox24@gmail.com>
2016-04-27 11:40:04 -0600

    dissolved shapes

---
+++ dissolve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Usage: Dissolve subPrecincts into Precincts in 2011 and 2012 folders
+shopt -s nullglob
+for f in 20**/*.geojson
+do
+  filename="${f%.*}"
+  echo "Dissolving shapes for - $filename"
+  ogr2ogr -f GeoJSON -explodecollections "$filename"Dissolve.geojson "$filename".geojson -dialect sqlite -sql "SELECT PrecinctID as PrecinctID, ST_Union(geometry) as geometry, CountyID as CountyID FROM OGRGeoJSON GROUP BY PrecinctID"
+done
