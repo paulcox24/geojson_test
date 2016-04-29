7c3f90e886cecb3cde769a3e1d3113c530922435
Paul Cox <paulcox24@gmail.com>
2016-04-28 19:05:00 -0600

    How about these

---
+++ valid_shapes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Usage: Fix weird shapes in dissolvedShapes 2011 and 2012 folders
+shopt -s nullglob
+for f in dissolvedShapes/20**/*Dissolve.geojson
+do
+  filename="${f%.*}"
+  echo "Cleaning up shapes for - $filename"
+  ogr2ogr -f GeoJSON "$filename"g.geojson "$filename".geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
+done
