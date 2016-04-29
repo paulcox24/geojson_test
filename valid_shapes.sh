ec8d750110e31238b93e08e8e2647b1f3a62c144
Paul Cox <paulcox24@gmail.com>
2016-04-29 16:55:40 -0600

    simple clean

--- valid_shapes.sh
+++ valid_shapes.sh
@@ -5,5 +5,5 @@ for f in dissolvedShapes/20**/*Dissolve.geojson
 do
   filename="${f%.*}"
   echo "Cleaning up shapes for - $filename"
-  ogr2ogr -f GeoJSON "$filename"g.geojson "$filename".geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
+  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"clean.geojson "$filename".geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
 done
