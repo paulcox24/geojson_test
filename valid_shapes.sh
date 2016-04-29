e968b341d353889ef4dbd05b394df7870a873b26
Paul Cox <paulcox24@gmail.com>
2016-04-29 17:56:30 -0600

    extra valid

--- valid_shapes.sh
+++ valid_shapes.sh
@@ -6,6 +6,8 @@ do
   filename="${f%.*}"
   echo "Cleaning up shapes for - $filename"
   ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"simple.geojson "$filename".geojson
-  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"clean.geojson "$filename"simple.geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
+  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"ssimple.geojson "$filename"simple.geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_SimplifyPreserveTopology(geometry, 0.0001) as geometry FROM OGRGeoJSON"
+  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"clean.geojson "$filename"ssimple.geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
   rm "$filename"simple.geojson
+  rm "$filename"ssimple.geojson
 done
