0f1b7c1be8a371e563232dd72770803767e319d0
Paul Cox <paulcox24@gmail.com>
2016-04-30 10:44:37 -0600

    another try

--- valid_shapes.sh
+++ valid_shapes.sh
@@ -5,9 +5,11 @@ for f in dissolvedShapes/20**/*Dissolve.geojson
 do
   filename="${f%.*}"
   echo "Cleaning up shapes for - $filename"
-  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"simple.geojson "$filename".geojson
-  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"ssimple.geojson "$filename"simple.geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_SimplifyPreserveTopology(geometry, 0.0001) as geometry FROM OGRGeoJSON"
-  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"clean.geojson "$filename"ssimple.geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
+  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"simple.geojson "$filename".geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_SimplifyPreserveTopology(geometry, 0.00001) as geometry FROM OGRGeoJSON"
+  ogr2ogr -f GeoJSON -Lco COORDINATE_PRECISION=12 "$filename"clean.geojson "$filename"simple.geojson -dialect sqlite -sql "SELECT PrecinctID, CountyID, ST_MakeValid(geometry) as geometry FROM OGRGeoJSON"
   rm "$filename"simple.geojson
-  rm "$filename"ssimple.geojson
 done
+mkdir cleaned/2011
+mkdir cleaned/2012
+mv dissolvedShapes/2011/*clean.geojson cleaned/2011
+mv dissolvedShapes/2012/*clean.geojson cleaned/2012
